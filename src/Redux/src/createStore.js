e830ce164995d2bc45c897c0956a4aa3ea7ab618
caoyw <17826856409@163.com>
2019-11-20 16:57:08 +0800

    add redux-createStore 解析

--- src/Redux/src/createStore.js
+++ src/Redux/src/createStore.js
@@ -90,37 +90,21 @@ export default function createStore(reducer, preloadedState, enhancer) {
       );
     }
 
+    // ????state???????redux?state??????????
     return currentState;
   }
 
   /**
-   * Adds a change listener. It will be called any time an action is dispatched,
-   * and some part of the state tree may potentially have changed. You may then
-   * call `getState()` to read the current state tree inside the callback.
-   *
-   * You may call `dispatch()` from a change listener, with the following
-   * caveats:
-   *
-   * 1. The subscriptions are snapshotted just before every `dispatch()` call.
-   * If you subscribe or unsubscribe while the listeners are being invoked, this
-   * will not have any effect on the `dispatch()` that is currently in progress.
-   * However, the next `dispatch()` call, whether nested or not, will use a more
-   * recent snapshot of the subscription list.
-   *
-   * 2. The listener should not expect to see all state changes, as the state
-   * might have been updated multiple times during a nested `dispatch()` before
-   * the listener is called. It is, however, guaranteed that all subscribers
-   * registered before the `dispatch()` started will be called with the latest
-   * state by the time it exits.
-   *
    * @param {Function} listener A callback to be invoked on every dispatch.
    * @returns {Function} A function to remove this change listener.
+   * subscribe & unsubscribe ???currenListener??????nextListeners??
    */
   function subscribe(listener) {
     if (typeof listener !== "function") {
       throw new Error("Expected the listener to be a function.");
     }
 
+    // ??????
     if (isDispatching) {
       throw new Error(
         "You may not call store.subscribe() while the reducer is executing. " +
@@ -132,6 +116,7 @@ export default function createStore(reducer, preloadedState, enhancer) {
 
     let isSubscribed = true;
 
+    // ???listeners
     ensureCanMutateNextListeners();
     nextListeners.push(listener);
 
@@ -155,31 +140,7 @@ export default function createStore(reducer, preloadedState, enhancer) {
     };
   }
 
-  /**
-   * Dispatches an action. It is the only way to trigger a state change.
-   *
-   * The `reducer` function, used to create the store, will be called with the
-   * current state tree and the given `action`. Its return value will
-   * be considered the **next** state of the tree, and the change listeners
-   * will be notified.
-   *
-   * The base implementation only supports plain object actions. If you want to
-   * dispatch a Promise, an Observable, a thunk, or something else, you need to
-   * wrap your store creating function into the corresponding middleware. For
-   * example, see the documentation for the `redux-thunk` package. Even the
-   * middleware will eventually dispatch plain object actions using this method.
-   *
-   * @param {Object} action A plain object representing “what changed”. It is
-   * a good idea to keep actions serializable so you can record and replay user
-   * sessions, or use the time travelling `redux-devtools`. An action must have
-   * a `type` property which may not be `undefined`. It is a good idea to use
-   * string constants for action types.
-   *
-   * @returns {Object} For convenience, the same action object you dispatched.
-   *
-   * Note that, if you use a custom middleware, it may wrap `dispatch()` to
-   * return something else (for example, a Promise you can await).
-   */
+  // action ?????? && action.type ?? && ??????dispatch????dispatch
   function dispatch(action) {
     if (!isPlainObject(action)) {
       throw new Error(
@@ -207,6 +168,7 @@ export default function createStore(reducer, preloadedState, enhancer) {
     }
 
     const listeners = (currentListeners = nextListeners);
+    // ??????????
     for (let i = 0; i < listeners.length; i++) {
       const listener = listeners[i];
       listener();
